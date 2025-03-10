c6aaabaf20e26c0c6a5dea1e07dd2b9defdb4b94
avagyan.armen <avagyan.armen@wb.ru>
2025-03-10 10:05:29 +0400

    change commit lesson 2

---
+++ javaLessons/src/main/java/Lesson2.java
@@ -0,0 +1,8 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+        System.out.println(5 & 8);
+        System.out.println(17 & 24);
+        System.out.println(5 | 8);
+        System.out.println(8 ^ 5);
+    }
+}
